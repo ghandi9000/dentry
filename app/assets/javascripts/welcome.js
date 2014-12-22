24f3713a5ed883c339551d4b56b208f59b0d2bd1
ghandi9000 <noah.v.peart@gmail.com>
2014-12-22 00:55:44 -0400

    google map added

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,11 @@
+function initialize() {
+    var mapOptions = {
+	center: { lat: 44.022746, lng: -71.830950},
+	zoom: 13,
+	mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+				  mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+alert("Hi!")
